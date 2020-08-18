7c093d2e8eee0262a519ccd93ebbef593ecc86cd
EbenBosman <eben.bosman@gmail.com>
2020-08-17 21:09:07 -0400

    include missing jquery

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 
-import '../src/styles/main.scss'
+import 'jquery/dist/jquery.slim';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../src/styles/main.scss';
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
