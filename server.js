61f075fc9e6d1e5208f74f2c03debb52583f2fbc
EbenBosman <eben.bosman@gmail.com>
2021-03-28 18:27:31 -0400

    Refactored encoding

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const serveStatic = require('serve-static');
 const server = express();
 
-server.get('*.js', function (req, res, next) {
-
+const setContentEncoding = (req, res) => {
     if (req.header("Accept-Encoding").indexOf('br') !== -1) {
         req.url = req.url + '.br';
         res.set('Content-Encoding', 'br');
@@ -11,35 +10,22 @@ server.get('*.js', function (req, res, next) {
         req.url = req.url + '.gz';
         res.set('Content-Encoding', 'gzip');
     }
+}
 
+server.get('*.js', function (req, res, next) {
+    setContentEncoding(req, res);
     res.set('Content-Type', 'text/javascript');
     next();
 });
 
 server.get('*.css', function (req, res, next) {
-    
-    if (req.header("Accept-Encoding").indexOf('br') !== -1) {
-        req.url = req.url + '.br';
-        res.set('Content-Encoding', 'br');
-    } else {
-        req.url = req.url + '.gz';
-        res.set('Content-Encoding', 'gzip');
-    }
-
+    setContentEncoding(req, res);
     res.set('Content-Type', 'text/css');
     next();
 });
 
 server.get('*.webp', function (req, res, next) {
-    
-    if (req.header("Accept-Encoding").indexOf('br') !== -1) {
-        req.url = req.url + '.br';
-        res.set('Content-Encoding', 'br');
-    } else {
-        req.url = req.url + '.gz';
-        res.set('Content-Encoding', 'gzip');
-    }
-
+    setContentEncoding(req, res);
     res.set('Content-Type', 'image/webp');
     next();
 });
