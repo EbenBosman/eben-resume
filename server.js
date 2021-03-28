f92c6d178ae996f9a9ff0421ff94f2cba7e9ddd4
EbenBosman <eben.bosman@gmail.com>
2021-03-28 16:44:38 -0400

    moved get to serve gzip

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const server = express();
-server.use(serveStatic(__dirname + "/public"));
-const port = process.env.PORT || 5000;
-server.listen(port);
 
 server.get('*.js', function (req, res, next) {
     req.url = req.url + '.gz';
     res.set('Content-Encoding', 'gzip');
+    res.set('Content-Type', 'text/javascript');
     next();
-})
+});
+
+server.get('*.css', function (req, res, next) {
+    req.url = req.url + '.gz';
+    res.set('Content-Encoding', 'gzip');
+    res.set('Content-Type', 'text/css');
+    next();
+});
+
+server.get('*.webp', function (req, res, next) {
+    req.url = req.url + '.gz';
+    res.set('Content-Encoding', 'gzip');
+    res.set('Content-Type', 'image/webp');
+    next();
+});
+
+server.use(serveStatic(__dirname + "/public"));
+const port = process.env.PORT || 5000;
+server.listen(port);
 
 console.log('server started ' + port);
