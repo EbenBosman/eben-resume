d299dc96ad911031ad4b2b67e5f7e83ddf1a3589
EbenBosman <eben.bosman@gmail.com>
2021-03-28 15:34:06 -0400

    add splitChunks to enable vendor chunks

--- webpack.config.js
+++ webpack.config.js
@@ -137,7 +137,28 @@ module.exports = params => {
 		},
 		optimization: {
 			minimize: isProduction,
-			minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin()]
-		}
+			minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin()],
+			splitChunks: {
+				chunks: 'async',
+				minSize: 20000,
+				minRemainingSize: 0,
+				minChunks: 1,
+				maxAsyncRequests: 30,
+				maxInitialRequests: 30,
+				enforceSizeThreshold: 50000,
+				cacheGroups: {
+					defaultVendors: {
+						test: /[\\/]node_modules[\\/]/,
+						priority: -10,
+						reuseExistingChunk: true,
+					},
+					default: {
+						minChunks: 2,
+						priority: -20,
+						reuseExistingChunk: true,
+					},
+				},
+			},
+		},
 	}
 }
