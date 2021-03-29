38e61a5b59c06e66d2c7654fa04332e638920e77
EbenBosman <eben.bosman@gmail.com>
2021-03-29 10:51:28 -0400

    terminal 404 pages

--- src/main-pages/pageNotFound.jsx
+++ src/main-pages/pageNotFound.jsx
@@ -1,38 +1,22 @@
 import React from 'react';
 
-const goBack = () => {
-    window.history.go(-1)
-}
-
-const goIndex = () => {
+const goHome = () => {
     window.location.href = '/'
 }
 
 const PageNotFound = () => (
     <React.Fragment>
-        <div style={{width: '100vw', height: '100vh', backgroundColor:"black"}}>
-            <figure>
-                <div className="sad-mac"></div>
-                <figcaption>
-                    <span className="sr-text">Error 404: Not Found</span>
-                    <span className="e"></span>
-                    <span className="r"></span>
-                    <span className="r"></span>
-                    <span className="o"></span>
-                    <span className="r"></span>
-                    <span className="_4"></span>
-                    <span className="_0"></span>
-                    <span className="_4"></span>
-                    <span className="n"></span>
-                    <span className="o"></span>
-                    <span className="t"></span>
-                    <span className="f"></span>
-                    <span className="o"></span>
-                    <span className="u"></span>
-                    <span className="n"></span>
-                    <span className="d"></span>
-                </figcaption>
-            </figure>
+        <div className="page-404-container">
+            <div className="page-404">
+                <div className="noise"></div>
+                <div className="overlay"></div>
+                <div className="terminal">
+                    <h1>Error <span className="errorcode">404</span></h1>
+                    <p className="output">The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+                    <p className="output">Please try to <a className="not-found" onClick={goHome}>return to the homepage</a>.</p>
+                    <p className="output">Good luck.</p>
+                </div>
+            </div>
         </div>
     </React.Fragment>
 );
