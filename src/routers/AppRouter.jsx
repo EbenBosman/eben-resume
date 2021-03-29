8c8131798f577a469d40929cc20bdb213b1410f2
EbenBosman <eben.bosman@gmail.com>
2021-03-29 11:26:24 -0400

    new loader

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -4,9 +4,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 const HomePage = lazy(/* webpackChunkName: "home-page" */() => import('../main-pages/home'));
 const PageNotFound = lazy(/* webpackChunkName: "page-not-found" */() => import('../main-pages/pageNotFound'));
 
+const loadingFallback = () => (
+    <div className="dot-spinner-container">
+        <div className="dot-spinner"></div>
+    </div>
+);
+
 const AppRouter = props => (
     <Router>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={loadingFallback()}>
             <Switch>
                 <Route path="/" component={HomePage} exact={true} />
                 <Route component={PageNotFound} />
