c5669f162582ab34926ead1b7796051391b59608
Eben Bosman <eben.bosman@gmail.com>
2022-10-14 19:59:41 -0400

    updated profile picture

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack');
 
 const CompressionPlugin = require("compression-webpack-plugin");
 const BrotliPlugin = require('brotli-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const WebpackBeforeBuildPlugin = require('before-build-webpack');
@@ -52,7 +52,10 @@ module.exports = params => {
 			rules: [{
 				test: /\.(js|jsx)$/,
 				loader: 'babel-loader',
-				exclude: /node_modules/
+				exclude: /node_modules/,
+                resolve: {
+                  fullySpecified: false,
+                }
 			},
 			{
 				test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
@@ -117,10 +120,10 @@ module.exports = params => {
 				ignoreOrder: true,
 			}),
 
-			new webpack.ProvidePlugin({
-				process: 'process/browser',
-				Buffer: ['buffer', 'Buffer'],
-			}),
+			// new webpack.ProvidePlugin({
+			// 	process: 'process/browser',
+			// 	Buffer: ['buffer', 'Buffer'],
+			// }),
 
 			new webpack.DefinePlugin({
 				'process.env': {
@@ -147,22 +150,22 @@ module.exports = params => {
 		],
 		devtool: isProduction ? 'nosources-source-map' : 'inline-source-map',	//	https://webpack.js.org/configuration/devtool/
 		devServer: {
-			contentBase: path.join(__dirname, 'public'),
+			static: path.join(__dirname, 'public'),
 			historyApiFallback: true,
 			https: true, // https://diary-of-programmer.blogspot.com/2019/04/tips-using-ssl-locally-with-webpack-dev.html
 			compress: true,
-			noInfo: false,
 			open: true,
 			port: 3000,
-			publicPath: '/assets/',
-			quiet: false,
-			stats: 'minimal', // none, errors-only, minimal, verbose
-			watchContentBase: true,
-			writeToDisk: true
+			hot: true,
+			devMiddleware: {
+				publicPath: '/assets/',
+				stats: 'minimal', // none, errors-only, minimal, verbose
+				writeToDisk: true
+			}
 		},
 		optimization: {
 			minimize: isProduction,
-			minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin()],
+			minimizer: [new TerserPlugin(), new CssMinimizerWebpackPlugin()],
 			splitChunks: {
 				chunks: 'async',
 				minSize: 20000,
