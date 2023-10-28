6124de064b836d630df6d4a608f229d85c2c6b56
Eben Bosman <eben.bosman@gmail.com>
2023-10-28 13:51:39 -0400

    attempt to improve ERR_HTTP_HEADERS_SENT error

--- server/resume-template.js
+++ server/resume-template.js
@@ -5,41 +5,39 @@ const pdf = require('html-pdf');
 module.exports = (res) => {
    fs.readFile('./src/data/resume.json', (err, resume_data) => {
       if (err) {
-         res.send(Promise.reject());
-      } else {
-         console.log('resume-template 1')
-         fs.readFile('./server/resume-template-components/template.hbs', (err, handlebarsTemplate) => {
+         return res.status(500).send('Error reading resume data');
+      }
+
+      fs.readFile('./server/resume-template-components/template.hbs', (err, handlebarsTemplate) => {
+         if (err) {
+            return res.status(500).send('Error reading template file');
+         }
+
+         const source = handlebarsTemplate.toString();
+
+         handlebars.registerHelper('replace', (str, toReplace, replaceWith) => {
+            return str ? str.replace(toReplace, replaceWith) : '';
+         });
+
+         const template = handlebars.compile(source);
+         const htmlString = template(JSON.parse(resume_data));
+
+         const options = {
+            "format": "A4"
+         };
+
+         // There exists an issue that if zoom is not applied to the html (inside the doc's style tag),
+         // then all content is rendered too large.
+         // https://github.com/marcbachmann/node-html-pdf/issues/110
+         pdf.create(htmlString, options).toFile('result.pdf', (err) => {
             if (err) {
-               res.send(Promise.reject());
+               console.error('Resume template error', err);
+               return res.status(500).send('Error creating PDF');
             }
 
-            const source = handlebarsTemplate.toString();
-
-            console.log('resume-template 0')
-            handlebars.registerHelper('replace', (str, toReplace, replaceWith) =>{
-               return str ? str.replace(toReplace, replaceWith) : '';
-            });
-
-            console.log('resume-template 1')
-            const template = handlebars.compile(source);
-            const htmlString = template(JSON.parse(resume_data));
-            console.log('resume-template 2', htmlString)
-
-            const options = {
-               "format": "A4"
-            };
-
-            // There exists an issue that if zoom is not applied to the html (inside the doc's style tag),
-            // then all content is rendered too large.
-            // https://github.com/marcbachmann/node-html-pdf/issues/110
-            pdf.create(htmlString, options).toFile('result.pdf', (err) => {
-               if (err) {
-                  res.send(Promise.reject());
-               }
-
-               res.send(Promise.resolve());
-            });
+            console.log('Resume template: Everything is fine');
+            res.status(200).send('PDF created successfully');
          });
-      }
+      });
    });
 };
