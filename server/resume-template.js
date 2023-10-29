c0252c5e65ece03adc23075dc53031b030d9d656
Eben Bosman <eben.bosman@gmail.com>
2023-10-29 14:46:23 -0400

    switch to html-pdf-node instead of html-pdf

--- server/resume-template.js
+++ server/resume-template.js
@@ -1,44 +1,43 @@
 const fs = require('fs');
 const handlebars = require('handlebars');
-const pdf = require('html-pdf');
+const pdf = require('html-pdf-node');
 
 module.exports = (req, res) => {
-   fs.readFile('./src/data/resume.json', (err, resume_data) => {
-      if (err) {
-         return res.status(500).send('Error reading resume data');
-      }
+    fs.readFile('./src/data/resume.json', (err, resume_data) => {
+        if (err) {
+            return res.status(500).send('Error reading resume data');
+        }
 
-      fs.readFile('./server/resume-template-components/template.hbs', (err, handlebarsTemplate) => {
-         if (err) {
-            return res.status(500).send('Error reading template file');
-         }
-
-         const source = handlebarsTemplate.toString();
-
-         handlebars.registerHelper('replace', (str, toReplace, replaceWith) => {
-            return str ? str.replace(toReplace, replaceWith) : '';
-         });
-
-         const template = handlebars.compile(source);
-         const htmlString = template(JSON.parse(resume_data));
-
-         const options = {
-            "format": "A4"
-         };
-
-         // There exists an issue that if zoom is not applied to the html (inside the doc's style tag),
-         // then all content is rendered too large.
-         // https://github.com/marcbachmann/node-html-pdf/issues/110
-         pdf.create(htmlString, options).toStream((err, stream) => {
+        fs.readFile('./server/resume-template-components/template.hbs', (err, handlebarsTemplate) => {
             if (err) {
-               console.error('PDF Create Error:', err);
-               return res.status(500).send('Error creating PDF');
+                return res.status(500).send('Error reading template file');
             }
 
-            res.setHeader('Content-Type', 'application/pdf');
-            res.setHeader('Content-Disposition', 'attachment; filename=result.pdf');
-            stream.pipe(res);  // Stream the PDF data to the response object
-         });
-      });
-   });
+            const source = handlebarsTemplate.toString();
+
+            handlebars.registerHelper('replace', (str, toReplace, replaceWith) => {
+                return str ? str.replace(toReplace, replaceWith) : '';
+            });
+
+            const template = handlebars.compile(source);
+            const htmlString = template(JSON.parse(resume_data));
+
+            const options = {
+                format: 'A4',
+            };
+
+            const file = {
+                content: htmlString
+            };
+
+            pdf.generatePdf(file, options).then(pdfBuffer => {
+                res.setHeader('Content-Type', 'application/pdf');
+                res.setHeader('Content-Disposition', 'attachment; filename=result.pdf');
+                res.send(pdfBuffer);
+            }).catch(error => {
+                console.error('Error generating PDF:', error);
+                res.status(500).send('Error generating PDF');
+            });
+        });
+    });
 };
